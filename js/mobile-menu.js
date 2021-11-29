10db85fb3aa26de9464de183d80a0c88c969517f
Ruslan Nikiforov <87496880+Leevorum@users.noreply.github.com>
2021-11-29 19:05:38 +0200

    костыли меню в портфолио

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -11,4 +11,11 @@
 
     mobileMenuRef.classList.toggle("is-open");
   });
+   function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+
+    const isModalClose = refs.modal.classList.contains("is-open");
+    const method = isModalClose ? "disableBodyScroll" : "enableBodyScroll";
+    bodyScrollLock[method](refs.modal);
+  }
 }) ();
