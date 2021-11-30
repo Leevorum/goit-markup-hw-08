7cc3c65cc8697d1575af82f5980b01b9a37f9aed
Leevorum <di4111ua@gmail.com>
2021-11-30 19:24:59 +0200

    fixed modal body

--- js/modal.js
+++ js/modal.js
@@ -4,11 +4,13 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
+  const mobileScroll = document.querySelector("[scroll-lock]");
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    mobileScroll.classList.toggle("stop-scroll");
   }
 })();
