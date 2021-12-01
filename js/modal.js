ca707d24a43b0a87de41ab0fecb038494ce67008
Leevorum <di4111ua@gmail.com>
2021-12-02 00:40:03 +0200

    fix modal desctop

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
-    mobileScroll.classList.toggle("stop-scroll");
+    // mobileScroll.classList.toggle("stop-scroll");
   }
 })();
